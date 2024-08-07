8ae2b0ce5000ed7b5227ec013791e2f2493c8a6d
Kevin <kevin@twjoin.com>
2024-08-07 16:04:33 +0800

    create project

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by 陳冠甫 on 2024/8/7.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
