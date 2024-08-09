5e34ffe4ff2b7e099f67b5ecb5cb93036152e40a
Kevin <kevin@twjoin.com>
2024-08-09 14:14:08 +0800

    basic setup list view

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -10,8 +10,28 @@ import SwiftUI
 struct AppetizerListView: View {
     var body: some View {
         NavigationStack {
-            Text("AppetizerListView")
-                .navigationTitle("🍿 Appetizers")
+            List(MockData.appetizers) { appetizer in
+                HStack {
+                    Image("")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .background(.brandPrimary)
+                        .frame(width: 120, height: 90)
+                        .cornerRadius(8)
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text(appetizer.name)
+                            .font(.title2)
+                            .fontWeight(.medium)
+                        
+                        Text("$\(appetizer.price, specifier: "%.2f")")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.semibold)
+                    }
+                    .padding(.leading)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("🍿 Appetizers")
         }
     }
 }
